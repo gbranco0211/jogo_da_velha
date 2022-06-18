2034ba2ee65a8d7bba3b716dda67a388252bad18
Guilherme Branco <87778836+gbranco0211@users.noreply.github.com>
2022-06-18 09:57:34 -0300

    Add files via upload

---
+++ jogo_da_velha/teste.py
@@ -0,0 +1,43 @@
+from string import octdigits
+from tkinter import *
+
+janela = Tk()
+janela.title("Jogo da Velha")
+
+botao1.Button(janela, text="Up", command=)
+botao1.grid(collum = 3, row = 1)
+
+botao2.Button(janela, text="Down", command=)
+botao2.grid(collum = 3, row = 3)
+
+botao3.Button(janela, text="Left", command=)
+botao3.grid(collum = 2, row = 2)
+
+botao4.Button(janela, text="Right", command=)
+botao4.grid(collum = 0, row = 2)
+
+botao5.Button(janela, text="Start", command=)
+botao5.grid(collum = 0, row = 0)
+
+botao6.Button(janela, text="End", command=)
+botao6.grid(collum = 0, row = 0)
+
+botaojv1.Button(janela, text="-", command=)
+botaojv1.grid(collum = 0, row = 0)
+
+botaojv2.Button(janela, text="-", command=)
+botaojv2.grid(collum = 0, row = 0)
+
+botaojv2.Button(janela, text="-", command=)
+botaojv2.grid(collum = 0, row = 0)
+
+botaojv2.Button(janela, text="-", command=)
+botaojv2.grid(collum = 0, row = 0)
+
+botaojv2.Button(janela, text="-", command=)
+botaojv2.grid(collum = 0, row = 0)
+
+botaojv2.Button(janela, text="-", command=)
+botaojv2.grid(collum = 0, row = 0)
+
+janela.mainloop()
